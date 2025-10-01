66debd434f02477b68b2177a7d205554ee2841ad
vantrujillo-dev <vany.trujillo@hotmail.com>
2025-10-01 10:50:23 -0600

    Se agregaron pinos animados
    se agregaron pinos animados, movimiento para apuntar con la bola y una camara dinámica

--- ASSETS/Practica 1/Scripts/ControlBola.cs
+++ ASSETS/Practica 1/Scripts/ControlBola.cs
@@ -4,23 +4,67 @@ using UnityEngine;
 
 public class ControlBola : MonoBehaviour
 {
+    public Transform CamaraPrincipal;
 
     public Rigidbody rb;
+
+    //Variables para apuntar 
+    public float velocidadDeApuntado = 5f;
+    public float limiteIzquierdo = -2f;
+    public float limiteDerecho = -2f;
+
+
     public float fuerzaDeLanzamiento = 1000f;
+
+    private bool haSidoLanzada = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Expresion: mientras que haSidoLanzada sea falso puedes disparar
+        if (haSidoLanzada == false)
         {
-
-            rb.AddForce(Vector3.forward * fuerzaDeLanzamiento);
+            Apuntar();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Lanzar();
+            }
         }
     }
-}
+
+    void Apuntar()
+    {
+        //Leer un input horizontal de tipo axis (eje) te permite registrar entradas con las teclas A y D, 
+        //Y flecha izquierda, y flecha derecha
+        float inputHorizontal = Input.GetAxis("Horizontal");
+
+        //Mover la bola hacia los lados
+        transform.Translate(Vector3.right * inputHorizontal * velocidadDeApuntado * Time.deltaTime);
+
+        //Delimitar el movimiento de la bola
+        Vector3 posicionActual = transform.position;
+
+        posicionActual.x = Mathf.Clamp(posicionActual.x, limiteIzquierdo, limiteDerecho);
+
+        transform.position = posicionActual;
+    }
+    void Lanzar()
+    {
+        haSidoLanzada = true;
+        rb.AddForce(Vector3.forward * fuerzaDeLanzamiento);
+        // si camara no es nulo, pasa a hacer la siguiente accion
+        if (CamaraPrincipal != null)
+        {
+            CamaraPrincipal.SetParent(transform);
+        }
+    }
+
+} //Bienvenido a la entrada al infierno
+
